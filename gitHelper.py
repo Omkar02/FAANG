40fb69e7704a61260c24adbe2234040229a58b84
Omkar02 <omkarjoshi4031@live.com>
2020-07-19 09:28:59 +0530

    Inillized Time logger

--- gitHelper.py
+++ gitHelper.py
@@ -8,10 +8,12 @@ import subprocess
 
 
 def gitStuff(message):
-    proc = subprocess.Popen(f'git statu')
-    proc = subprocess.Popen(f'git add .')
-    proc = subprocess.Popen(f'git commit -m "{message}"')
-    proc = subprocess.Popen(f'git push -u origin master')
+    proc = subprocess.Popen(f'git status')
+    proc = subprocess.Popen(f'git add -A')
+    proc = subprocess.Popen(f'git status')
+
+    # proc = subprocess.Popen(f'git commit -m "{message}"')
+    # proc = subprocess.Popen(f'git push -u origin master')
 
 
 # print ('result: ', proc.communicate())
