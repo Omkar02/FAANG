14d8ac9dafa98f80529b09b01ef4a80ab3e42ae1
Omkar02 <omkarjoshi4031@live.com>
2020-09-01 22:03:21 +0530

    170

--- LC_1423_MaximumPointsYouCanObtainfromCards.py
+++ LC_1423_MaximumPointsYouCanObtainfromCards.py
@@ -3,4 +3,46 @@ from Helper.TimerLogger import CodeTimeLogging
 fileName = main.__file__
 fileName = fileName.split('\\')[-1]
 
-CodeTimeLogging(Flag='S', filename=fileName, Tag='Array', Difficult='Medium')
+CodeTimeLogging(Flag='F', filename=fileName, Tag='Array', Difficult='Medium')
+
+
+def maxScore(cardPoints, k):
+    start = curIdx = 0
+    maxScore = float('-inf')
+    ele = []
+    cnt = 0
+    n = len(cardPoints)
+    while cnt < k + 1:
+        print(curIdx, '====')
+        currSum = 0
+        theElement = []
+        for i in range(start, start + k):
+            curIdx = i
+            # print('--->', i)
+            theElement.append(cardPoints[i])
+            currSum += cardPoints[i]
+        print('\t', currSum, theElement)
+        if currSum > maxScore:
+            maxScore = currSum
+            ele = theElement
+        start -= 1
+
+        cnt += 1
+        if cnt == len(cardPoints):
+            break
+    return maxScore
+
+
+cardPoints = [1, 2, 3, 4, 5, 6, 1]
+k = 3
+cardPoints = [9, 7, 7, 9, 7, 7, 9]
+k = 7
+cardPoints = [1, 1000, 1]
+k = 1
+cardPoints = [1, 79, 80, 1, 1, 1, 200, 1]
+k = 3
+cardPoints = [11, 49, 100, 20, 86, 29, 72]
+k = 4
+cardPoints = [1, 2, 3, 4, 5, 6, 1]
+k = 3
+print(maxScore(cardPoints, k))
---
+++ LC_752_OpentheLock.py
@@ -0,0 +1,56 @@
+import __main__ as main
+from Helper.TimerLogger import CodeTimeLogging
+fileName = main.__file__
+fileName = fileName.split('\\')[-1]
+
+CodeTimeLogging(Flag='F', filename=fileName, Tag='Graph', Difficult='Medium')
+
+
+# start = list('00000')
+# cnt = 0
+# idx = 0
+# n = len(start)
+# while cnt < 1000 and idx < n:
+#     print(''.join(start), end=' ')
+#     start[idx] = str(int(start[idx]) + 1)
+#     # start = f'{start[0:idx]}{str(currNum)}{start[idx:n-1]}'
+#     if int(start[idx]) == 9:
+#         print()
+#         idx += 1
+#     cnt += 1
+
+cntq = [0]
+
+
+def openLock(deadends, target):
+    q = [(0, '0000')]
+    deadends = set(deadends)
+
+    if target == '0000':
+        return -1
+
+    while q:
+        step, lock = q.pop(0)
+
+        for i in range(4):
+            cntq[0] += 1
+            prv = lock[:i] + str((int(lock[i]) - 1) % 10) + lock[i + 1:]
+            nxt = lock[:i] + str((int(lock[i]) + 1) % 10) + lock[i + 1:]
+
+            if prv == target or nxt == target:
+                return step + 1
+
+            if prv not in deadends:
+                q.append((step + 1, prv))
+                deadends.add(prv)
+
+            if nxt not in deadends:
+                q.append((step + 1, nxt))
+                deadends.add(nxt)
+    return -1
+
+
+deadends = ["0201", "0101", "0102", "1212", "2002"]
+target = "0202"
+print(openLock(deadends, target))
+print(cntq)
